f10cdf0b68b2cee341422c6c2699856ccc8a627c
Jerry Lee <oldratlee@gmail.com>
2023-07-11 00:51:27 +0800

    initial commit

---
+++ scripts/integration_test.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -eEuo pipefail
+cd "$(dirname "$(readlink -f "$0")")"
+
+BASH_BUDDY_ROOT="$(readlink -f bash-buddy)"
+readonly BASH_BUDDY_ROOT
+source "$BASH_BUDDY_ROOT/lib/trap_error_info.sh"
+source "$BASH_BUDDY_ROOT/lib/common_utils.sh"
+source "$BASH_BUDDY_ROOT/lib/java_build_utils.sh"
+
+################################################################################
+# ci build logic
+################################################################################
+
+readonly default_build_jdk_version=17
+# shellcheck disable=SC2034
+readonly JDK_VERSIONS=(
+  8
+  11
+  "$default_build_jdk_version"
+  20
+  21
+)
+readonly default_jh_var_name="JAVA${default_build_jdk_version}_HOME"
+
+# here use `install` and `-D performRelease` intended
+#   to check release operations.
+#
+# De-activate a maven profile from command line
+#   https://stackoverflow.com/questions/25201430
+#
+# shellcheck disable=SC2034
+readonly JVB_MVN_OPTS=(
+  "${JVB_DEFAULT_MVN_OPTS[@]}"
+  -DperformRelease -P'!gen-sign'
+  ${CI_MORE_MVN_OPTS:+${CI_MORE_MVN_OPTS}}
+)
+
+cd ..
+
+########################################
+# build and test by default version jdk
+########################################
+
+[ -d "${!default_jh_var_name:-}" ] || cu::die "\$${default_jh_var_name}(${!default_jh_var_name:-}) dir is not existed!"
+export JAVA_HOME="${!default_jh_var_name}"
+
+cu::head_line_echo "build and test with Java $default_build_jdk_version: $JAVA_HOME"
+jvb::mvn_cmd clean install
+
+########################################
+# test by multiply version jdks
+########################################
+
+for jdk_version in "${JDK_VERSIONS[@]}"; do
+  # skip default jdk, already tested above
+  [ "$jdk_version" = "$default_build_jdk_version" ] && continue
+
+  jh_var_name="JAVA${jdk_version}_HOME"
+  [ -d "${!jh_var_name:-}" ] || cu::die "\$${jh_var_name}(${!jh_var_name:-}) dir is not existed!"
+  export JAVA_HOME="${!jh_var_name}"
+
+  # just test without build
+  cu::head_line_echo "test with Java $jdk_version: $JAVA_HOME"
+
+  # about CI env var
+  #   https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
+  if [ "${CI:-}" = true ]; then
+    jvb::mvn_cmd jacoco:prepare-agent surefire:test jacoco:report
+  else
+    jvb::mvn_cmd surefire:test
+  fi
+done
